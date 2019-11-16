294dfd1db3f9ccde82304f1c019604f0f8bb7368
k2Raven <leha.21.05.1996@gmail.com>
2019-11-16 16:01:43 +0600

    Этап №2 просмотр товара

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ProductView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('products/<int:pk>/', ProductView.as_view(), name='product_detail'),
+
 ]
