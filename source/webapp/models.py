2b23773dd07638a5bd15b2d2169220fc0a9c9f75
k2Raven <leha.21.05.1996@gmail.com>
2019-11-16 14:30:30 +0600

    Этап №1 модели товара и отзыва

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -14,3 +16,12 @@ class Product(models.Model):
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Фото')
 
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User,  on_delete=models.CASCADE, null=True, blank=True, verbose_name='Автор')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Товар')
+    text_review = models.TextField(max_length=3000, verbose_name='Текст отзыва')
+    rating =models.IntegerField(verbose_name='Оценка')
